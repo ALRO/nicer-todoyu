da55119552a30d88932e3d661e2cfae6044e6f89
Alexander Rothmund <arothmund@snowflake.ch>
2014-09-29 18:50:13 +0200

    select full task nr on click

--- js/todoyu.js
+++ js/todoyu.js
@@ -1,3 +1,20 @@
 var a = chrome.extension.getURL("css/todoyu.css");
 
 $('<link rel="stylesheet" type="text/css" href="' + a + '" >').appendTo("head");
+
+$('body').on('click', '.taskNumber', function(e) {
+    var text = e.currentTarget;
+    var range, selection;
+
+    if (document.body.createTextRange) {
+        range = document.body.createTextRange();
+        range.moveToElementText(text);
+        range.select();
+    } else if (window.getSelection) {
+        selection = window.getSelection();
+        range = document.createRange();
+        range.selectNodeContents(text);
+        selection.removeAllRanges();
+        selection.addRange(range);
+    }
+});
