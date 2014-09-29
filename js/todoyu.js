110db31968af921703a998f59f2bb470b5245eed
Alexander Rothmund <alexander@rothmund.ch>
2014-09-29 20:53:35 +0200

    refactoring and stuff

--- js/todoyu.js
+++ js/todoyu.js
@@ -1,20 +1,43 @@
-var a = chrome.extension.getURL("css/todoyu.css");
+"use strict";
 
-$('<link rel="stylesheet" type="text/css" href="' + a + '" >').appendTo("head");
+var nicerTodoyu = {};
 
-$('body').on('click', '.taskNumber', function(e) {
-    var text = e.currentTarget;
-    var range, selection;
+/**
+ * Initialize all custom stuff
+ */
+nicerTodoyu.init = function(){
+    nicerTodoyu.loadCss();
+    nicerTodoyu.clickableTaskNumbers();
+};
 
-    if (document.body.createTextRange) {
-        range = document.body.createTextRange();
-        range.moveToElementText(text);
-        range.select();
-    } else if (window.getSelection) {
-        selection = window.getSelection();
-        range = document.createRange();
-        range.selectNodeContents(text);
-        selection.removeAllRanges();
-        selection.addRange(range);
-    }
-});
+
+/**
+ *  Appends css files to header
+ */
+nicerTodoyu.loadCss = function(){
+    var todoyuCssUrl = chrome.extension.getURL("css/todoyu.css");
+    $('<link rel="stylesheet" type="text/css" href="' + todoyuCssUrl + '" >').appendTo("head");
+};
+
+
+/**
+ *  Makes task numbers select on click for easy copying
+ */
+nicerTodoyu.clickableTaskNumbers = function(){
+    $('body').on('click', '.taskNumber', function(e) {
+        var text = e.currentTarget;
+        var range, selection;
+
+        if (document.body.createTextRange) {
+            range = document.body.createTextRange();
+            range.moveToElementText(text);
+            range.select();
+        } else if (window.getSelection) {
+            selection = window.getSelection();
+            range = document.createRange();
+            range.selectNodeContents(text);
+            selection.removeAllRanges();
+            selection.addRange(range);
+        }
+    });
+};
